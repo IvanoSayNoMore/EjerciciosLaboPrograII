1aa08301816eb11a5a2803daf413e0815ee020ab
ivan Fabella <fabellaivan@hotmail.com>
2022-09-05 20:16:31 -0300

    Actualizacion Ejercicio Estantes

--- Productos/Estante.cs
+++ Productos/Estante.cs
@@ -8,9 +8,78 @@ namespace Productos
 {
     public class Estante
     {
-        private Producto productos = new Producto();
-        private int ubicacionEstante; 
 
+        private Producto[] _productos;
+        private int _ubicacionEstante;
 
+        public Estante(int capacidad)
+        {
+            _productos = new Producto[capacidad];
+        }
+        public Estante(int capacidad,int ubicacion) : this(capacidad)
+        {
+           _ubicacionEstante = ubicacion;
+        }
+
+        Producto[] GetProductos()
+        {
+            return _productos;
+        }
+        public static string MostrarEstante(Estante e)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Estante ubicacion: {e._ubicacionEstante} \n");
+
+            for (int i = 0; i < e._productos.Length; i++)
+            {
+                if (!Object.ReferenceEquals(e._productos[i], null))
+                {
+                    sb.AppendLine(Producto.MostrarProducto(e._productos[i]));
+                    sb.AppendLine("----------------------");
+
+                }
+            }
+            return sb.ToString();
+        }
+
+        #region SOBRECARGAS
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            for(int i = 0; i < e._productos.Length; i++)
+            {
+                if(e._productos[i] == p)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            if (e != p)
+            {
+                for (int i = 0; i < e._productos.Length; i++)
+                {
+                    if (e._productos[i] is null)
+                    {
+                        e._productos[i] = p;
+                        return true;
+
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        #endregion SOBRECARGAS
     }
 }
--- Productos/Producto.cs
+++ Productos/Producto.cs
@@ -8,11 +8,100 @@ namespace Productos
 {
     public class Producto
     {
-        private string codigoDeBarra;
-        private string marca;
-        private float precio;
+        private string _codigoDeBarra;
+        private string _marca;
+        private float  _precio;
 
+        public Producto(string marca, string codigoDeBarra, float precio)
+        {
+            this._codigoDeBarra  = codigoDeBarra;
+            this._marca          = marca;
+            this._precio         = precio;
+        }
 
+        public static string MostrarProducto(Producto p)
+        {
+            return String.Format("MARCA {0} CODIGO {1} PRECIO {2} ", p._marca, (string)p, p._precio);
+        }
+
+        /*************************************************************/
+        #region GetterSetter
+
+        public string GetMarca
+        {
+            get { return this._marca; }
+        }
+
+        public float GetPrecio
+        {
+            get { return this._precio; }
+        }
+        #endregion GetterSetter
+
+        /*************************************************************/
+
+        #region SOBRECARGAS Operadores
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            if (!(p1 is null || p2 is null))
+            {
+                return (p1.GetMarca == p2.GetMarca && p1._codigoDeBarra == p2._codigoDeBarra);
+            }
+            return false;
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            if (!(p1 is null || p2 is null))
+            {
+                return !(p1.GetMarca == p2.GetMarca);
+            }
+            return false;
+        }
+  
+
+        public static bool operator ==(Producto p1, string marca)
+        {
+            if (!(p1 is null || marca is null))
+            {
+                if (p1.GetMarca == marca)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool operator !=(Producto p1, string marca)
+        {
+            if (!(p1 is null || marca is null))
+            {
+                if (p1.GetMarca == marca)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        #endregion SOBRECARGAS Operadores
+
+        /*************************************************************/
+
+        #region ImplicitasExplicitas
+
+        public static implicit operator string(Producto p1)
+        {
+            if(!(p1 is null))
+            {
+                return p1._codigoDeBarra;
+            }
+            return "alg";
+        }
+
+        #endregion ImplicitasExplicitas
+
+        /*************************************************************/
 
     }
 }
